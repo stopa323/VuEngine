16ca1fc92293210b0c59a9e8849770c22245805c
Bartosz Stopa <stopa323@gmail.com>
2017-02-04 15:45:03 +0100

    Handle mouse buttons events
    Closes #6

--- Engine/InputManager/FInputManager.cpp
+++ Engine/InputManager/FInputManager.cpp
@@ -20,13 +20,117 @@ FInputManager::~FInputManager() {
 }
 
 void FInputManager::ProcessInput() {
+	// Todo: handle all input in one call (while loop)
+	_input_event = xcb_poll_for_event( _connection );
+	if ( nullptr == _input_event ) {
+		free( _input_event );
+		return;
+	}
 
+	EMouseEvent mouse_event = EMouseEvent::UNKNOWN;
+
+	/* Switch on event type */
+	switch ( _input_event->response_type & ~0x80 ) {
+	case XCB_BUTTON_PRESS: {
+		mouse_event = getMouseButtonPressEvent();
+	} break;
+	case XCB_BUTTON_RELEASE: {
+		mouse_event = getMouseButtonReleaseEvent();
+	} break;
+	case XCB_MOTION_NOTIFY: {
+		std::cout << "XCB_MOTION_NOTIFY" << std::endl;
+	} break;
+	case XCB_KEY_PRESS: {
+		std::cout << "XCB_KEY_PRESS" << std::endl;
+	} break;
+	case XCB_KEY_RELEASE: {
+		std::cout << "XCB_KEY_RELEASE" << std::endl;
+	} break;
+	}
+
+	/* Free generic event */
+	free( _input_event );
 }
 
 EMouseEvent FInputManager::getMouseButtonPressEvent() {
+	xcb_button_press_event_t* press_event = (xcb_button_press_event_t*) _input_event;
+	uint16_t event_modifiers = press_event->state;
+
+	/* Substract unused modifiers */
+	event_modifiers &= (MOUSE_BUTTON_MOD_CAPSLOCK^0xffff);
+	event_modifiers &= (MOUSE_BUTTON_MOD_NUMLOCK^0xffff);
+
+	if ( (MOUSE_BUTTON_MOD_SHIFT) == event_modifiers ) {
+		if ( MOUSE_BUTTON_LEFT 			== press_event->detail ) return EMouseEvent::LMB_SHIFT_PRESS;
+		if ( MOUSE_BUTTON_MIDDLE 		== press_event->detail ) return EMouseEvent::MMB_SHIFT_PRESS;
+		if ( MOUSE_BUTTON_RIGHT 		== press_event->detail ) return EMouseEvent::RMB_SHIFT_PRESS;
+		if ( MOUSE_BUTTON_WHEEL_UP 		== press_event->detail ) return EMouseEvent::WHEEL_UP_SHIFT;
+		if ( MOUSE_BUTTON_WHEEL_DOWN 	== press_event->detail ) return EMouseEvent::WHEEL_DOWN_SHIFT;
+	}
+
+	if ( (MOUSE_BUTTON_MOD_CTRL) == event_modifiers ) {
+		if ( MOUSE_BUTTON_LEFT 			== press_event->detail ) return EMouseEvent::LMB_CTRL_PRESS;
+		if ( MOUSE_BUTTON_MIDDLE 		== press_event->detail ) return EMouseEvent::MMB_CTRL_PRESS;
+		if ( MOUSE_BUTTON_RIGHT 		== press_event->detail ) return EMouseEvent::RMB_CTRL_PRESS;
+		if ( MOUSE_BUTTON_WHEEL_UP 		== press_event->detail ) return EMouseEvent::WHEEL_UP_CTRL;
+		if ( MOUSE_BUTTON_WHEEL_DOWN 	== press_event->detail ) return EMouseEvent::WHEEL_DOWN_CTRL;
+	}
+
+	if ( (MOUSE_BUTTON_MOD_LALT) == event_modifiers ) {
+		// Note: its not working, window does not handle this event
+		std::cout << "NOT IMPLEMENTED" << std::endl;
+	}
+
+	if ( (MOUSE_BUTTON_MOD_SHIFT_CTRL) == event_modifiers ) {
+		if ( MOUSE_BUTTON_LEFT 			== press_event->detail ) return EMouseEvent::LMB_SHIFT_CTRL_PRESS;
+		if ( MOUSE_BUTTON_MIDDLE 		== press_event->detail ) return EMouseEvent::MMB_SHIFT_CTRL_PRESS;
+		if ( MOUSE_BUTTON_RIGHT 		== press_event->detail ) return EMouseEvent::RMB_SHIFT_CTRL_PRESS;
+		if ( MOUSE_BUTTON_WHEEL_UP 		== press_event->detail ) return EMouseEvent::WHEEL_UP;
+		if ( MOUSE_BUTTON_WHEEL_DOWN 	== press_event->detail ) return EMouseEvent::WHEEL_DOWN;
+	}
+
+	if ( (MOUSE_BUTTON_MOD_SHIFT_LALT) == event_modifiers ) {
+		if ( MOUSE_BUTTON_LEFT 			== press_event->detail ) return EMouseEvent::LMB_SHIFT_LALT_PRESS;
+		if ( MOUSE_BUTTON_MIDDLE 		== press_event->detail ) return EMouseEvent::MMB_SHIFT_LALT_PRESS;
+		if ( MOUSE_BUTTON_RIGHT 		== press_event->detail ) return EMouseEvent::RMB_SHIFT_LALT_PRESS;
+		if ( MOUSE_BUTTON_WHEEL_UP 		== press_event->detail ) return EMouseEvent::WHEEL_UP;
+		if ( MOUSE_BUTTON_WHEEL_DOWN 	== press_event->detail ) return EMouseEvent::WHEEL_DOWN;
+	}
+
+	if ( (MOUSE_BUTTON_MOD_CTRL_LALT) == event_modifiers ) {
+		if ( MOUSE_BUTTON_LEFT 			== press_event->detail ) return EMouseEvent::LMB_CTRL_LALT_PRESS;
+		if ( MOUSE_BUTTON_MIDDLE 		== press_event->detail ) return EMouseEvent::MMB_CTRL_LALT_PRESS;
+		if ( MOUSE_BUTTON_RIGHT 		== press_event->detail ) return EMouseEvent::RMB_CTRL_LALT_PRESS;
+		if ( MOUSE_BUTTON_WHEEL_UP 		== press_event->detail ) return EMouseEvent::WHEEL_UP;
+		if ( MOUSE_BUTTON_WHEEL_DOWN 	== press_event->detail ) return EMouseEvent::WHEEL_DOWN;
+	}
+
+	if ( (MOUSE_BUTTON_MOD_SHIFT_CTRL_LALT) == event_modifiers ) {
+		if ( MOUSE_BUTTON_LEFT 			== press_event->detail ) return EMouseEvent::LMB_SHIFT_CTRL_LALT_PRESS;
+		if ( MOUSE_BUTTON_MIDDLE 		== press_event->detail ) return EMouseEvent::MMB_SHIFT_CTRL_LALT_PRESS;
+		if ( MOUSE_BUTTON_RIGHT 		== press_event->detail ) return EMouseEvent::RMB_SHIFT_CTRL_LALT_PRESS;
+		if ( MOUSE_BUTTON_WHEEL_UP 		== press_event->detail ) return EMouseEvent::WHEEL_UP;
+		if ( MOUSE_BUTTON_WHEEL_DOWN 	== press_event->detail ) return EMouseEvent::WHEEL_DOWN;
+	}
+
+	/* Treat any other combination as no modifiers */
+	if ( MOUSE_BUTTON_LEFT 			== press_event->detail ) return EMouseEvent::LMB_PRESS;
+	if ( MOUSE_BUTTON_MIDDLE 		== press_event->detail ) return EMouseEvent::MMB_PRESS;
+	if ( MOUSE_BUTTON_RIGHT 		== press_event->detail ) return EMouseEvent::RMB_PRESS;
+	if ( MOUSE_BUTTON_WHEEL_UP 		== press_event->detail ) return EMouseEvent::WHEEL_UP;
+	if ( MOUSE_BUTTON_WHEEL_DOWN 	== press_event->detail ) return EMouseEvent::WHEEL_DOWN;
+
 	return EMouseEvent::UNKNOWN;
 }
 
 EMouseEvent FInputManager::getMouseButtonReleaseEvent() {
+	xcb_button_release_event_t* press_event = (xcb_button_release_event_t*) _input_event;
+
+	if ( MOUSE_BUTTON_LEFT 			== press_event->detail ) return EMouseEvent::LMB_RELEASE;
+	if ( MOUSE_BUTTON_MIDDLE 		== press_event->detail ) return EMouseEvent::MMB_RELEASE;
+	if ( MOUSE_BUTTON_RIGHT 		== press_event->detail ) return EMouseEvent::RMB_RELEASE;
+	if ( MOUSE_BUTTON_WHEEL_UP 		== press_event->detail ) return EMouseEvent::WHEEL_UP;
+	if ( MOUSE_BUTTON_WHEEL_DOWN 	== press_event->detail ) return EMouseEvent::WHEEL_DOWN;
+
 	return EMouseEvent::UNKNOWN;
 }
